521582898a7d4d5c6ca306b10bb627e98a5cdb71
vadim <vadyukkha@mail.ru>
2023-09-26 11:43:14 +0300

    test new func

--- fuctorial.cpp
+++ fuctorial.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int factorial(int x){
@@ -7,8 +7,19 @@ int factorial(int x){
     return x+(factorial(x-1));
 };
 
+int factNR(int x) {
+    vector<int> temp = {0};
+    for (int i = 1; i <= x; i++) {
+        int a = temp[temp.size() - 1] + i;
+        temp.push_back(a);
+    }
+    return temp[ temp.size() - 1 ];
+};
+
 int main(){
     int result, x = 10;
     cout << factorial(x) << endl;
+
+    cout << factNR(x) << endl;
     return 0;
 }
